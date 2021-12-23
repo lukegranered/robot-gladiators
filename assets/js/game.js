846ff542b84913b0ec32f121312228664a5aa754
Luke G <lukejgranered@gmail.com>
2021-12-23 14:47:23 -0600

    player name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,8 +10,19 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -123,6 +134,7 @@ var startGame = function(){
     for (var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
         window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+        debugger;
 
             var pickedEnemyObj = enemyInfo[i];
 
